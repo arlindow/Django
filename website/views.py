7018704fba28c591cef3b57b43650f365ef28bdb
arlindo wilson <arlindowilsonaws@gmail.com>
2026-01-04 11:30:44 -0300

    -view-funcionarios

--- website/views.py
+++ website/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from helloworld.models import Funcionario 
 
 # Create your views here.
+def lista_funcionarios(request):
+    funcionarios = Funcionario.objetos.all()
+
+    contexto = {
+        'funcionarios': funcionarios
+    }
+
+    return render(
+        request,
+        "funcionarios.html",
+        contexto
+    )
+
+def index(request):
+    return render(request, 'funcionarios.html')
